5c45e50c01db57246e18852e387410b1cbf42d8e
Lightweight Mobile NoSQL Database <iboxdb@163.com>
2015-12-06 02:31:23 -0800

    dnxcore5

---
+++ dnxcore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello iBoxDB!");
+            //iBoxDB.LocalServer.DB.Root("/tmp/");
+            var text = iBoxDB.TestHelper.RunALL();
+            Console.WriteLine(text); 
+        }
+    }
+}
