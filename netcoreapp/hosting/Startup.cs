cf4397d601343301afc0fcae772d3667decc473e
Fast transactional nosql Application Database <iboxdb@users.noreply.github.com>
2016-09-26 19:03:27 +0800

    asp.net core

---
+++ netcoreapp/hosting/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+	        iBoxDB.LocalServer.DB.Root("/tmp/");
+	        var text =  iBoxDB.NDB.RunALL();
+                await context.Response.WriteAsync(text);
+            });
+        }
+    }
+}
