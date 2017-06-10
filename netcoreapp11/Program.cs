22fa723f98699f4be0a2666810055cb177f822bc
Fast transactional nosql Application Database <iboxdb@users.noreply.github.com>
2017-06-10 10:05:04 +0800

    Add files via upload

---
+++ netcoreapp11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hwapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+   iBoxDB.LocalServer.DB.Root("/tmp/");
+  var text =  iBoxDB.NDB.RunALL(true);
+  Console.WriteLine(text);
+            Console.WriteLine("Hello World! NET1.0");
+        }
+    }
+}
